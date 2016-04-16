c6f4dce1b1c00ab576db9817ef34919808cc9aef
Vicky Gray <vickygray26@hotmail.com>
2016-04-16 21:25:51 +0100

    avergage of array

--- lib/questions.rb
+++ lib/questions.rb
@@ -110,6 +110,7 @@ end
 # get the average from an array, rounded to the nearest integer
 # so [10, 15, 25] should return 17
 def average_of_array(array)
+  (array.inject { |sum, x| sum + x }.to_f/array.length).round
 end
 
 # get all the elements in an array, up until the first element

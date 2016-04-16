b7721e1d1d6ab2c090ef42c31da8b21b1bcc26b4
Vicky Gray <vickygray26@hotmail.com>
2016-04-16 20:51:04 +0100

    separate array into odd and even numbers

--- lib/questions.rb
+++ lib/questions.rb
@@ -67,6 +67,9 @@ end
 # even numbers come first
 # so [1, 2, 3, 4, 5, 6] becomes [[2, 4, 6], [1, 3, 5]]
 def separate_array_into_even_and_odd_numbers(array)
+  result = []
+  result << evens = array.select { |x| x.even? }
+  result << odds = array.select { |x| x.odd? }
 end
 
 # count the numbers of elements in an element which are palindromes

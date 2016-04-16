2d3be2c6636c24e3f2f721c0815ce015c8870e1e
Vicky Gray <vickygray26@hotmail.com>
2016-04-16 22:40:41 +0100

    get all letters in array of words

--- lib/questions.rb
+++ lib/questions.rb
@@ -135,6 +135,7 @@ end
 # . e.g. the array ['cat', 'dog', 'fish'] becomes
 # ['a', 'c', 'd', 'f', 'g', 'h', 'i', 'o', 's', 't']
 def get_all_letters_in_array_of_words(array)
+  array.map { |word| word.split(//) }.flatten.sort
 end
 
 # swap the keys and values in a hash. e.g.

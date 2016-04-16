d723010db1ba7b9d4f4058d9a81ccea522bdc4c4
Vicky Gray <vickygray26@hotmail.com>
2016-04-17 00:34:00 +0100

    format date nicely

--- lib/questions.rb
+++ lib/questions.rb
@@ -165,11 +165,13 @@ end
 # round down a float up and convert it to an Integer,
 # so 9.52 becomes 9
 def round_down_number(float)
+  float.floor
 end
 
 # take a date and format it like dd/mm/yyyy, so Halloween 2013
 # becomes 31/10/2013
 def format_date_nicely(date)
+  date.strftime("%d/%m/%Y")
 end
 
 # get the domain name *without* the .com part, from an email address

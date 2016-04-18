4d8d32934ce1d1d8b6ddd0004d9c071523b5ffcb
Vicky Gray <vickygray26@hotmail.com>
2016-04-18 11:45:20 +0100

    add together keys and values

--- lib/questions.rb
+++ lib/questions.rb
@@ -149,6 +149,8 @@ end
 # add all the keys and all the values together, e.g.
 # {1 => 1, 2 => 2} becomes 6
 def add_together_keys_and_values(hash)
+  hash.flatten.inject { |sum, x| sum + x }
+
 end
 
 # take out all the capital letters from a string

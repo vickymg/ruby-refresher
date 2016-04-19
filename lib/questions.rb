9d8d8ae9e1c63879b2bc055f9945eb3362341428
Vicky Gray <vickygray26@hotmail.com>
2016-04-19 18:03:06 +0100

    is a 2014 bank holiday?

--- lib/questions.rb
+++ lib/questions.rb
@@ -239,6 +239,8 @@ end
 # the list of bank holidays is here:
 # https://www.gov.uk/bank-holidays
 def is_a_2014_bank_holiday?(date)
+  bank_holidays = ["2014/05/26", "2014/08/25", "2014/12/25", "2014/12/26"]
+  bank_holidays.include? date.strftime("%Y/%m/%d")
 end
 
 # given your birthday this year, this method tells you

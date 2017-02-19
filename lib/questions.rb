2ddd8245d1e87824b9e71757e79503ad2125634b
Vicky Gray <vickygray26@hotmail.com>
2017-02-19 17:13:14 +0000

    Select elements starting with a vowel

--- lib/questions.rb
+++ lib/questions.rb
@@ -5,6 +5,7 @@ end
 
 # keep only the elements that start with a vowel
 def select_elements_starting_with_vowel(array)
+	array.select { |item| item if item[0].match /[aeiou]/ }
 end
 
 # remove instances of nil (but NOT false) from an array

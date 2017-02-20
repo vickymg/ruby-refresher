93bf400cb1b0094d264e518fe6852b016f8f6d8d
Vicky Gray <vickygray26@hotmail.com>
2017-02-20 21:13:16 +0000

    Remove nils and false from array

--- lib/questions.rb
+++ lib/questions.rb
@@ -15,6 +15,7 @@ end
 
 # remove instances of nil AND false from an array
 def remove_nils_and_false_from_array(array)
+	array. compact.reject { |item| item == false}
 end
 
 # don't reverse the array, but reverse every word inside it. e.g.

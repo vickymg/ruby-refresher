d9c622325964a952f33f213f7ed825c77889101c
Vicky Gray <vickygray26@hotmail.com>
2017-03-06 22:57:07 +0000

    Sort array by last letter of word

--- lib/questions.rb
+++ lib/questions.rb
@@ -45,6 +45,7 @@ end
 # sort an array of words by their last letter, e.g.
 # ['sky', 'puma', 'maker'] becomes ['puma', 'maker', 'sky']
 def array_sort_by_last_letter_of_word(array)
+	array.sort_by { | word| word[-1] }
 end
 
 # cut strings in half, and return the first half, e.g.
